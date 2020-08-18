2ebc183abda1dacc8d3cc7cd6d8ef6d17a1b14da
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-18 10:37:16 +0300

    cashier

--- cashier.py
+++ cashier.py
@@ -1,5 +1,27 @@
 def main():
-	# write code here
+    item = 0
+    list = []
+    total = 0
+
+    while item != "done":
+        items = {}
+        item = input("Enter item name: ")
+        if item != "done":
+            price = int(input("Enter item price: "))
+            quantity = int(input("Enter item quantity: "))
+            items["name"] = item
+            items["price"] = price
+            items["quantity"] = quantity
+            list.append(items)
+
+    for x in list:
+        total += x["price"] * x["quantity"]
+
+    for x in list:
+        print(x["quantity"] , x["name"] , x["price"] * x["quantity"])
+
+    print("Total Price: " , total)
+
 
 if __name__ == '__main__':
-	main()
+    main()
